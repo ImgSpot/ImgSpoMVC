d078a9c0bcf82e40e250e4254b165b3d4cf55007
ross clark <rosshclark24@gmail.com>
2021-05-21 20:30:53 -0400

    added functionality to apiconnectd Nothing hooked up yet)

--- ImgSpot.Client/Controllers/UserController.cs
+++ ImgSpot.Client/Controllers/UserController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ImgSpot.Client.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace ImgSpot.Client.Controllers
 {
@@ -20,7 +22,18 @@ namespace ImgSpot.Client.Controllers
 
         public IActionResult Index()
         {
-            return View("index");
+            var client = new HttpClient();
+            var response = client.GetAsync($"{_logger["Services:webapi"]}").GetAwaiter().GetResult();
+            List<string> result = null;
+
+            if(response.IsSuccessStatusCode)
+            {
+                result = JsonConvert.DeserializeObject<List<string>>(response.Content.ReadAsStringAsync().GetAwaiter);
+                ViewBag.Object = result;
+                return View("index");
+            }
+            return null;
+            
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
